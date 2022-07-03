c0c6a3361f68c1be505a92db75009ed5d83aad56
DaichiSaito <hrkedz@gmail.com>
2022-07-03 16:10:57 +0900

    一通り実装した

---
+++ spec/factories/spaces.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: spaces
+#
+#  id              :bigint           not null, primary key
+#  address         :string(255)
+#  description     :text(65535)      not null
+#  latitude        :float(24)
+#  longitude       :float(24)
+#  name            :string(255)      not null
+#  nearest_station :string(255)
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+FactoryBot.define do
+  factory :space do
+    name { 'MyString' }
+    description { 'MyText' }
+    address { 'MyString' }
+    nearest_station { 'MyString' }
+  end
+end
